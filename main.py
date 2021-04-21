b65ac1c49afc3f8c3ae0e87681f46d30cb08e9c0
brendon <brendon@localhost.localdomain>
2021-04-21 17:57:11 -0600

    Updated main to allow for disabling external controls

--- main.py
+++ main.py
@@ -14,17 +14,26 @@ RESTART_DELAY_SECS = 3
 main_logger = setup_logger("main", "main.log")
 
 
-def main() -> None:
+def main(use_controls: bool = True) -> None:
     with ManagedAudioPlayer() as player:
-        controls.main_control_loop(player)
+        print("Not using controls")
+        if use_controls:
+            controls.main_control_loop(player)
+        else:
+            print("Not using controls")
+            while True:
+                song_finished = player.play_current_song()  # Will block during playback
+                if song_finished:
+                    player.next_song()
+                sleep(controls.SONG_BREAK_DELAY_SECS)
 
 
-def resilient_main() -> None:
+def resilient_main(use_controls: bool = True) -> None:
     """Will attempt to recover from failure to maintain uptime.
     Allow for exiting via keyboard interrupts."""
     while True:
         try:
-            main()
+            main(use_controls)
         except Exception as e:
             main_logger.warning(f"Music Pi service died with error: {e}. Restarting...")
         except KeyboardInterrupt:
@@ -34,11 +43,14 @@ def resilient_main() -> None:
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-r", "--resilient", action="store_true")
+    parser.add_argument("-r", "--resilient", action="store_true",
+                        help="Will restart itself on failure until a keyboard interrupt is received.")
+    parser.add_argument("-n", "--no-controls", action="store_false",
+                        help="Disables controls")
     args = parser.parse_args()
 
     if args.resilient:
-        resilient_main()
+        resilient_main(args.no_controls)
     else:
-        main()
+        main(args.no_controls)
 
